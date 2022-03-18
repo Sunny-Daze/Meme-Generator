34c741f6d0355b284093705815e18e53cb632b6a
Sunny-Daze7 <sunnyadav0727@gmail.com>
2022-03-18 13:15:38 +0530

    Initial Commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+    return (
+      <header>
+        <img
+          src="https://c.tenor.com/GryShD35-psAAAAC/troll-face-creepy-smile.gif"
+          alt="problem?"
+        ></img>
+        <p>Meme Generator</p>
+      </header>
+    );
+}
+
+export default Header;
